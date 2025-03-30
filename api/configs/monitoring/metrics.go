ff1080377e7ab6acf135d22322994227cd49d5ce
João Anderson <joao.anderson.af@gmail.com>
2025-03-30 17:18:53 -0300

    configurando grafana e criando dashboards

--- api/configs/monitoring/metrics.go
+++ api/configs/monitoring/metrics.go
@@ -6,23 +6,23 @@ import (
 
 var (
 	RequestCount200 = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "http_requests_200",
+		Name: "albums_http_requests_200",
 		Help: "Total number of HTTP requests successfully served.",
 	}, []string{"path", "method", "status"})
 	RequestCount400 = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "http_requests_400",
+		Name: "albums_http_requests_400",
 		Help: "Total number of HTTP requests not successfully served because of a client error.",
 	}, []string{"path", "method", "status"})
 	RequestCount500 = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "http_requests_500",
+		Name: "albums_http_requests_500",
 		Help: "Total number of HTTP requests not successfully served because of a server error.",
 	}, []string{"path", "method", "status"})
 	RequestCountTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "http_requests_total",
+		Name: "albums_http_requests_total",
 		Help: "Total number of HTTP requests made.",
 	}, []string{})
 	RequestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    "http_request_duration",
+		Name:    "albums_http_request_duration",
 		Help:    "HTTP request latencies in seconds.",
 		Buckets: prometheus.DefBuckets,
 	}, []string{"path", "method"})
