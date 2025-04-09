3cddd0fe4ecccbd707dae821e5635fedaba22f6c
João Anderson <joao.anderson.af@gmail.com>
2025-04-09 10:48:53 -0300

    ajustando configurações dos dashboards

--- k8s/albums-monitoring/pvc_configmap/grafana-dashboards.sh
+++ k8s/albums-monitoring/pvc_configmap/grafana-dashboards.sh
@@ -3,5 +3,5 @@ DASHBOARD_PATH=$1
 kubectl create configmap grafana-dashboards \
   --from-file="${DASHBOARD_PATH}/dashboards.yml" \
   --from-file="${DASHBOARD_PATH}/albums_http_requests.json" \
-  --from-file="${DASHBOARD_PATH}/go_metrics.json" \
+  --from-file="${DASHBOARD_PATH}/metrics_go.json" \
   -n albums-monitoring
